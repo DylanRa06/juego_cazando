c38ee2f67c6dbc59962f36263fd3b0ba0e74a5f8
DylanRa06 <dylanramones20000@hotmail.com>
2026-06-28 11:50:30 -0500

    Parte 2: Definir variables, constantes y funcion iniciarJuego para posicionar gato y comida

---
+++ cazando.js
@@ -0,0 +1,33 @@
+let canvas = document.getElementById("areaJuego");
+let ctx = canvas.getContext("2d");
+
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+const ANCHO_GATO = 50;
+const ALTO_GATO = 50;
+const ANCHO_COMIDA = 20;
+const ALTO_COMIDA = 20;
+
+function iniciarJuego() {
+    gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
+    gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
+    
+    comidaX = canvas.width - ANCHO_COMIDA;
+    comidaY = canvas.height - ALTO_COMIDA;
+
+    graficarGato();
+    graficarComida();
+}
+
+function graficarGato() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
+}
+
+function graficarComida() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
+}
