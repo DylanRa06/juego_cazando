a1dd8e38430be7148196032d24879b6097e34387
DylanRa06 <dylanramones20000@hotmail.com>
2026-06-28 15:58:20 -0500

    Parte 5: cuenta regresiva con condiciones de fin de juego

--- cazando.js
+++ cazando.js
@@ -10,30 +10,11 @@ const ANCHO_GATO = 50;
 const ALTO_GATO = 50;
 const ANCHO_COMIDA = 20;
 const ALTO_COMIDA = 20;
+
 let puntos = 0;
+let tiempo = 10;
+let intervaloTiempo;
 
-function detectarColision() {
-    if (gatoX < comidaX + ANCHO_COMIDA &&
-        gatoX + ANCHO_GATO > comidaX &&
-        gatoY < comidaY + ALTO_COMIDA &&
-        gatoY + ALTO_GATO > comidaY) {
-        
-        alert("¡El gato atrapó la comida!");
-        
-        puntos += 1;
-        document.getElementById("puntos").innerText = puntos;
-        
-        generarComidaAleatoria();
-    }
-}
-
-function generarComidaAleatoria() {
-    comidaX = Math.floor(Math.random() * (canvas.width - ANCHO_COMIDA));
-    comidaY = Math.floor(Math.random() * (canvas.height - ALTO_COMIDA));
-    limpiarCanva();
-    graficarGato();
-    graficarComida();
-}
 function iniciarJuego() {
     gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
     gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
@@ -43,6 +24,8 @@ function iniciarJuego() {
 
     graficarGato();
     graficarComida();
+
+    intervaloTiempo = setInterval(restarTiempo, 1000);
 }
 
 function graficarGato() {
@@ -63,33 +46,78 @@ function limpiarCanva() {
 }
 
 function moverIzquierda() {
-    gatoX -= 10;
-    limpiarCanva();
-    graficarGato();
-    graficarComida();
-    detectarColision();
+    if (tiempo > 0 && puntos < 6) {
+        gatoX -= 10;
+        limpiarCanva();
+        graficarGato();
+        graficarComida();
+        detectarColision();
+    }
 }
 
 function moverDerecha() {
-    gatoX += 10;
-    limpiarCanva();
-    graficarGato();
-    graficarComida();
-    detectarColision();
+    if (tiempo > 0 && puntos < 6) {
+        gatoX += 10;
+        limpiarCanva();
+        graficarGato();
+        graficarComida();
+        detectarColision();
+    }
 }
 
 function moverArriba() {
-    gatoY -= 10;
-    limpiarCanva();
-    graficarGato();
-    graficarComida();
-    detectarColision();
+    if (tiempo > 0 && puntos < 6) {
+        gatoY -= 10;
+        limpiarCanva();
+        graficarGato();
+        graficarComida();
+        detectarColision();
+    }
 }
 
 function moverAbajo() {
-    gatoY += 10;
+    if (tiempo > 0 && puntos < 6) {
+        gatoY += 10;
+        limpiarCanva();
+        graficarGato();
+        graficarComida();
+        detectarColision();
+    }
+}
+
+function detectarColision() {
+    if (gatoX < comidaX + ANCHO_COMIDA &&
+        gatoX + ANCHO_GATO > comidaX &&
+        gatoY < comidaY + ALTO_COMIDA &&
+        gatoY + ALTO_GATO > comidaY) {
+        
+        puntos += 1;
+        cambiarTexto("puntos", puntos);
+        
+        if (puntos >= 6) {
+            clearInterval(intervaloTiempo);
+            alert("¡Eres el GANADOR!");
+            return;
+        }
+        
+        generarComidaAleatoria();
+    }
+}
+
+function generarComidaAleatoria() {
+    comidaX = Math.floor(Math.random() * (canvas.width - ANCHO_COMIDA));
+    comidaY = Math.floor(Math.random() * (canvas.height - ALTO_COMIDA));
     limpiarCanva();
     graficarGato();
     graficarComida();
-    detectarColision();
+}
+
+function restarTiempo() {
+    tiempo -= 1;
+    cambiarTexto("tiempo", tiempo);
+
+    if (tiempo <= 0) {
+        clearInterval(intervaloTiempo);
+        alert("GAME OVER");
+    }
 }
